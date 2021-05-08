783266921eaa45952874c8c34004cf6ed6290366
Kendall <ktrudickm@gmail.com>
2021-05-08 09:53:20 -0400

    Last edits before presentation

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -8,6 +8,7 @@ router.post('/', withAuth, async (req, res) => {
       ...req.body,
       user_id: req.session.user_id,
     });
+    console.log(createPost)
     res.status(200).json(createPost);
   } catch (err) {
     res.status(400).json(err);
